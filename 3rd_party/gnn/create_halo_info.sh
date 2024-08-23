9348f257bf779b1f18d8ba93e66667a5dc9c3684
Shivam <sbarwey@polaris-login-04.hsn.cm.polaris.alcf.anl.gov>
2024-08-23 05:09:28 +0000

    SB: modified run scripts

--- 3rd_party/gnn/create_halo_info.sh
+++ 3rd_party/gnn/create_halo_info.sh
@@ -17,16 +17,3 @@ DATA_PATH=/lus/eagle/projects/datascience/sbarwey/codes/nek/nekRS-ML-GNN/example
 
 echo Running create_halo_info.py 
 python create_halo_info.py --SIZE $PROCS --POLY ${POLY} --PATH ${DATA_PATH} 
-
-#HALO_SWAP_MODE=none
-#HALO_SWAP_MODE=all_to_all
-HALO_SWAP_MODE=all_to_all_opt
-EXE=./main.py
-ARGS="backend=nccl halo_swap_mode=${HALO_SWAP_MODE} gnn_outputs_path=${DATA_PATH}"
-
-echo Running script $EXE
-echo with arguments $ARGS
-echo
-echo `date`
-mpiexec --envall -n $PROCS --ppn $PROCS_PER_NODE --cpu-bind=list:24:16:8:1 python $EXE ${ARGS}
-echo `date`
