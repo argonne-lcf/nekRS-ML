455776e904510a66e6c7982af8a14777e58d1e2f
balin <riccardo.balin@colorado.edu>
2024-08-30 15:54:24 +0000

    Added launcher processes to drover script for concurrent simulation launches. Needs testing on >1 simulaiton nodes and fix for SmartSim status issue

--- examples/workflow_sketch/run_train_clustered.sh
+++ examples/workflow_sketch/run_train_clustered.sh
@@ -11,6 +11,9 @@ export TORCH_PATH=$( python -c 'import torch; print(torch.__path__[0])' )
 export LD_LIBRARY_PATH=$TORCH_PATH/lib:$LD_LIBRARY_PATH
 
 # Run the driver script
-python ssim_driver.py 
+# 2 nodes
+python ssim_driver.py --sim_nodes=1 --db_nodes=1 --simprocs=1 --simprocs_pn=1
+#python ssim_driver.py --sim_nodes=1 --db_nodes=1 --simprocs=2 --simprocs_pn=2
+#python ssim_driver.py --sim_nodes=1 --db_nodes=1 --simprocs=4 --simprocs_pn=4
 
 
--- examples/workflow_sketch/ssim_driver.py
+++ examples/workflow_sketch/ssim_driver.py
@@ -5,11 +5,14 @@ import sys
 #import hydra
 from argparse import ArgumentParser
 import numpy as np
+import multiprocessing as mp
+from time import sleep
 
 # smartsim and smartredis imports
 from smartsim import Experiment
+#import smartsim.status as ssim_status
+import smartsim
 from smartsim.settings import RunSettings, PalsMpiexecSettings
-#from smartredis import Client
 from smartredis import Client, Dataset
 
 ## Define function to parse node list
@@ -22,6 +25,37 @@ def parseNodeList(fname):
     return nodelist, nNodes
 
 
+## Launch a SmartSim Model for NekRS
+def launch_nrs(launch_id, node_list, gpu_list, run_settings, experiment):
+    #  (I think here we can use queues to send stopping events when training is done)
+    #  (We can get stuff from queue at every iteration of the loop and if it meets some event we stop the model and break the loop)
+    
+    # Launch the first model
+    run_id = 0
+    run_settings.set_hostlist(','.join(node_list))
+    nrs_model = experiment.create_model(f"nekrs_{launch_id}_{run_id}", run_settings)
+    print(f"Launching a new NekRS simulation from launcher process {launch_id} on nodes {node_list} and GPUs {gpu_list}",flush=True)
+    experiment.generate(nrs_model, overwrite=True)
+    experiment.start(nrs_model, summary=False, block=False)
+    
+    # Check its status and launch another one when finished
+    
+    while True:
+        sleep(5)
+        status = experiment.get_status(nrs_model)[0]
+        if status==smartsim.status.SmartSimStatus.STATUS_NEW:
+            print(f'nekrs_{launch_id} status: New',flush=True)
+        elif status==smartsim.status.SmartSimStatus.STATUS_RUNNING:
+            print(f'nekrs_{launch_id} status: Running',flush=True)
+        elif status==smartsim.status.SmartSimStatus.STATUS_COMPLETED:
+            # launch a new model
+            run_id+=1
+            print(f"Launching a new NekRS simulation from launcher process {launch_id} on nodes {node_list} and GPUs {gpu_list}",flush=True)
+            #nrs_model = experiment.create_model(f"nekrs_{launch_id}_{run_id}", run_settings)
+            #experiment.generate(nrs_model, overwrite=True)
+            #experiment.start(nrs_model, summary=False, block=False)
+            break
+
 
 ## Clustered DB launch
 def launch_clDB(args, nodelist, nNodes):
@@ -29,6 +63,7 @@ def launch_clDB(args, nodelist, nNodes):
     dbNodes_list = None
     if (nodelist is not None):
         simNodes = ','.join(nodelist[0: args.sim_nodes])
+        simNodes_list = nodelist[0: args.sim_nodes]
         dbNodes = ','.join(nodelist[args.sim_nodes: \
                                 args.sim_nodes + args.db_nodes])
         dbNodes_list = nodelist[args.sim_nodes: \
@@ -83,23 +118,35 @@ def launch_clDB(args, nodelist, nNodes):
                                            env_vars={'SSDB' : SSDB})
         nrs_settings.set_tasks(args.simprocs)
         nrs_settings.set_tasks_per_node(args.simprocs_pn)
-        nrs_settings.set_hostlist(simNodes)
 
-    # Launch a few simulations
-    # Try:
-    #    - using own logic, SmartSim API and mp.Process to manage different models and lauch them on resources
-    for isim in range(1):
-        nrs_model = exp.create_model(f"nekrs_{isim}", nrs_settings)
+    # Launch the NekRS launcher processes
+    n_gpu_pn = 4
+    n_concurrent_nrs_runs = int((args.sim_nodes*n_gpu_pn)/args.simprocs)
+    n_nodes_nrs_runs = float(args.simprocs/n_gpu_pn)
+    #print(f'{n_nodes_nrs_runs=}')
+    processes = []
+    print(f'\nSetting up {n_concurrent_nrs_runs} NekRS launcher processes',flush=True)
+    for launch_id in range(n_concurrent_nrs_runs):
+        if args.sim_nodes==1:
+            process_node_list = list(simNodes_list)
+        else:
+            node_id_start = (n_nodes_nrs_runs*launch_id)//args.sim_nodes
+            node_id_end = node_id_start+int(n_nodes_nrs_runs)
+            #print(f'{launch_id}: {node_id_start} - {node_id_end}')
+        if args.simprocs_pn==n_gpu_pn:
+            gpu_list = [i for i in range(n_gpu_pn)]
+        else:
+            gpu_list = [int(i+n_nodes_nrs_runs*n_gpu_pn*launch_id) for i in range(args.simprocs_pn)]
+            #print(f'{launch_id}: gpu {gpu_list}')
+        p = mp.Process(target=launch_nrs, args=(launch_id, process_node_list, gpu_list, nrs_settings, exp))
+        processes.append(p)
 
-        # Start the client model
-        print(f"Launching the NekRS simulation number {isim} ...")
-        block = False
-        #if len(args.sim_copy_files)>0 or len(args.sim_link_files)>0:
-        #    nrs_model.attach_generator_files(to_copy=list(args.sim_copy_files), to_symlink=list(args.sim_link_files))
-        exp.generate(nrs_model, overwrite=True)
-        exp.start(nrs_model, summary=False, block=block)
-        print("Done\n")
-   
+    print(f'Starting the NekRS launcher processes',flush=True)
+    for p in processes:
+        p.start()
+    print("Done\n")
+
+
     # Launch a trainer 
     if (launcher=='pals'):
         SSDB = db.get_address()[0]
@@ -118,6 +165,13 @@ def launch_clDB(args, nodelist, nNodes):
     exp.start(train_model, summary=False, block=block)
     print("Done\n")
 
+    # Join the launcher processes
+    print("Joining the launcher processes",flush=True)
+    for p in processes:
+        p.join()
+        print("Joined 1 process",flush=True)
+    print("Done\n",flush=True)
+
     # Stop database
     print("Stopping the Orchestrator ...")
     exp.stop(db)
@@ -132,8 +186,8 @@ def main():
     parser = ArgumentParser(description='Online training from NekRS ensembles')
     parser.add_argument('--sim_nodes', default=1, type=int, help='Number of nodes assigned to the simulations')
     parser.add_argument('--db_nodes', default=1, type=int, help='Number of nodes assigned to the database')
-    parser.add_argument('--simprocs_pn', default=4, type=int, help='Number of MPI processes per node for simulation')
-    parser.add_argument('--simprocs', default=4, type=int, help='Number of MPI processes for simulation')
+    parser.add_argument('--simprocs_pn', default=1, type=int, help='Number of MPI processes per node for simulation')
+    parser.add_argument('--simprocs', default=1, type=int, help='Number of MPI processes for simulation')
     args = parser.parse_args()
 
     # Get nodes of this allocation (job)
