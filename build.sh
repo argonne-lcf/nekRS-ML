70253bff197ec3c49abcb557e0907f02992f46c3
Riccardo Balin <riccardobalin@Riccardos-MacBook-Pro.local>
2025-05-07 16:00:42 -0600

    Update build scripts

--- build.sh
+++ build.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 set -a
 
-module load cmake
-
-CC=mpicc
-CXX=mpic++
-FC=mpif77
-
-BUILD_DIR=$PWD/build
-INSTALL_DIR=$HOME/.local/nekrs
-
-if [ -d ${BUILD_DIR} ]; then
-  rm -r ${BUILD_DIR}
+if [ -z ${CC} ] || [ -z ${CXX}  ] || [ -z ${FC}  ]; then
+  echo -e "\033[31mWARNING: env-var CC, CXX or FC is not set!\033[m"
+  export CC=mpicc
+  export CXX=mpic++
+  export FC=mpif77
+  read -p         "Press ENTER to continue with CC=$CC CXX=$CXX FC=$FC or ctrl-c to cancel"
 fi
 
+if [[ -z "$BUILD_DIR" ]]; then
+  BUILD_DIR=$PWD/build
+fi
+if [ -d ${BUILD_DIR} ]; then
+  #rm -r ${BUILD_DIR}
+  echo -e "\n\033[31mWARNING! Found a build directory already, you may want to clean it.\033[m\n"
+fi
+
+if [[ -z "$INSTALL_DIR" ]]; then
+  INSTALL_DIR=$HOME/.local/nekrs
+fi
 if [ -d ${INSTALL_DIR} ]; then
-  rm -r ${INSTALL_DIR}
+  #rm -r ${INSTALL_DIR}
+  echo -e "\n\033[31mWARNING! Found an install directory already, you may want to clean it.\033[m\n"
 fi
 
 cmake -S . -B ${BUILD_DIR} -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}  -Wfatal-errors && \
 cmake --build ${BUILD_DIR} --parallel 8 && \
 cmake --install ${BUILD_DIR}
+if [ $? -eq 0 ]; then
+  echo ""
+  echo -e "\033[35mHooray! You're all set. The installation is complete.\033[m"
+  echo ""
+fi
