83bfb3de2c9563f4228369c55b91883f3a25b2fd
balin <riccardo.balin@colorado.edu>
2025-07-23 22:00:02 +0000

    (WIP): still debugging udf file for turbChannel_wallModel example

--- examples/turbChannel_wallModel/run.sh
+++ examples/turbChannel_wallModel/run.sh
@@ -6,15 +6,21 @@ echo Jobid: $PBS_JOBID
 echo Running on host `hostname`
 echo Running on nodes `cat $PBS_NODEFILE`
 module restore
-module load frameworks
+module use /soft/modulefiles/
+module load conda
+conda activate
 module list
 
 export NEKRS_HOME=/home/balin/.local/nekrs/
-export OCCA_DPCPP_COMPILER_FLAGS="-O3 -fsycl -fsycl-targets=intel_gpu_pvc -ftarget-register-alloc-mode=pvc:auto -fma"
-export FI_CXI_RX_MATCH_MODE=hybrid
-export UR_L0_USE_COPY_ENGINE=0
+export NEKRS_CACHE_BCAST=0
+export NEKRS_LOCAL_TMP_DIR=/local/scratch
+export NEKRS_GPU_MPI=0
+export MPICH_MPIIO_HINTS=
+export MPICH_MPIIO_STATS=0
+export MPICH_GPU_SUPPORT_ENABLED=0
+export MPICH_OFI_NIC_POLICY=NUMA
 
 # Run nekRS
-mpiexec -n 2 -ppn 2 --cpu-bind=list:1:8:16:24:32:40:53:60:68:76:84:92 -- /home/balin/.local/nekrs//bin/nekrs --setup turbChannel --backend dpcpp
+mpiexec -n 2 -ppn 2 --cpu-bind=list:24:16:8:1 -- /home/balin/.local/nekrs//bin/nekrs --setup turbChannel --backend CUDA
 
 # Post-process
