4f3d31d834ff13986a36f0f4fe7407a4185d8e24
Thilina Ratnayaka <thilinarmtb@users.noreply.github.com>
2026-01-21 13:47:49 -0800

    Various fixes for Aurora gitlab CI runs (#48)
    * Minor refactor
    
    * Switch to debug queue
    
    * Switch to Performance temporarily
    
    * Remove unused version variable
    
    * Add `commit` variable to configure the build
    
    * Update `README.md`
    
    * Use `prod` queue for reframe tests
    
    * Run `all` tag by default
    
    * Remove `NekRSKershawTest`

--- tests/tests.py
+++ tests/tests.py
@@ -5,34 +5,6 @@ import os
 
 
 @rfm.simple_test
-class NekRSKershawTest(NekRSTest):
-    num_nodes = parameter([1])
-    ranks_per_node = parameter([2])
-
-    def __init__(self):
-        super().__init__(
-            "kershaw", "kershaw", nn=self.num_nodes, rpn=self.ranks_per_node
-        )
-
-    # Match "flops/rank" at start of line to avoid matching output during setup.
-    @performance_function("flops/s/rank", perf_key="BPS5")
-    def bps5_performance(self):
-        return sn.extractsingle(r"^flops/rank: (\S+)", self.stdout, 1, float, 0)
-
-    @performance_function("flops/s/rank", perf_key="BP5")
-    def bp5_performance(self):
-        return sn.extractsingle(r"^flops/rank: (\S+)", self.stdout, 1, float, 1)
-
-    @performance_function("flops/s/rank", perf_key="BP6")
-    def bp6_performance(self):
-        return sn.extractsingle(r"^flops/rank: (\S+)", self.stdout, 1, float, 2)
-
-    @performance_function("flops/s/rank", perf_key="BP6PCG")
-    def bp6pcg_performance(self):
-        return sn.extractsingle(r"^flops/rank: (\S+)", self.stdout, 1, float, 3)
-
-
-@rfm.simple_test
 class NekRSTGVOffline(NekRSMLOfflineTest):
     num_nodes = parameter([1])
     ranks_per_node = parameter([2])
