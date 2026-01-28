Fix bug with loss computation, testing new step embeddings

3rd_party/dist-dgn/gnn.py
@@ -22,15 +22,19 @@ class DistributedDGN(torch.nn.Module):
         self.parse_arch(arch)
 
         # ~~~~ Diffusion-step embedding
+        freq_width = max(self.mlp_hidden_channels, 128)
+        emb_width = max(self.mlp_hidden_channels, freq_width*2)
         self.diffusion_step_embedding = nn.Sequential(
-            SinusoidalPositionEmbedding(self.mlp_hidden_channels),
-            nn.Linear(self.mlp_hidden_channels, self.emb_width),
-            nn.SELU(),
+            SinusoidalPositionEmbedding(freq_width),
+            nn.Linear(freq_width, 2 * emb_width),
+            nn.SiLU(),
+            nn.Linear(2 * emb_width, emb_width),
+            nn.SiLU()
         )
 
         # ~~~~ Diffusion-step encoder
         self.diffusion_step_encoder = nn.ModuleList([
-            nn.Linear(self.emb_width, self.mlp_hidden_channels),
+            nn.Linear(emb_width, self.mlp_hidden_channels),
             nn.SELU(), 
             nn.Linear(self.mlp_hidden_channels * 2, self.mlp_hidden_channels),
         ])
@@ -71,7 +75,7 @@ class DistributedDGN(torch.nn.Module):
             self.processor.append(
                         DistributedMessagePassingLayer(
                                      channels = self.mlp_hidden_channels,
-                                     emb_features = self.emb_width,
+                                     emb_features = emb_width,
                                      n_mlp_hidden_layers = self.n_mlp_hidden_layers,
                                      halo_swap_mode = self.halo_swap_mode, 
                                      layer_norm = self.layer_norm,
@@ -120,6 +124,7 @@ class DistributedDGN(torch.nn.Module):
 
         # ~~~~ Embed the diffusion step
         emb = self.diffusion_step_embedding(r) # Shape (batch_size, emb_width)
+
 
         # ~~~~ Node encoder
         x = torch.cat([field_r, cond_node_features], dim=1) if cond_node_features is not None else field_r

3rd_party/dist-dgn/step_sampler.py
@@ -40,7 +40,7 @@ class StepSampler():
         return samples, importance_weights
     
 
-class UniformStepSampler(StepSampler):
+class UniformSampler(StepSampler):
     """Uniform sampler for the diffusion steps"""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -74,7 +74,7 @@ class AdaptiveExponentialSampler(StepSampler):
     def __init__(self, 
         initial_decay_rate=0.0001, 
         final_decay_rate=0.03,
-        decay_rate_increment=0.05, # 5% increment per step
+        decay_rate_increment=0.01, # 1% increment per step
         *args, 
         **kwargs
     ):
@@ -85,12 +85,11 @@ class AdaptiveExponentialSampler(StepSampler):
     
     @property
     def weights(self) -> torch.Tensor:
-        timesteps = torch.arange(self.num_diffusion_steps, dtype=self.dtype)
-
         # Compute the adaptive decay rate
         self.decay_rate = self.initial_decay_rate * (1 + self.decay_rate_increment) ** self.step
         self.decay_rate = min(self.decay_rate, self.final_decay_rate)
 
         # Compute the weights
+        timesteps = torch.arange(self.num_diffusion_steps, dtype=self.dtype)
         weights = torch.exp(-self.decay_rate * timesteps).to(self.device)
         return weights

3rd_party/dist-dgn/trainer.py
@@ -1125,10 +1125,17 @@ class DGNTrainer:
                                                         r, 
                                                         batch=data.batch, 
                                                         dirichlet_mask=BC_mask)
-        if self.cfg.postprocess and self.iteration%100 == 0:
-            postprocess.plot_2d_field(COMM, graph.pos, field_r[data.batch==0].cpu().numpy(), f'field_r_r{r[0]}.png')
-            postprocess.plot_2d_field(COMM, graph.pos, data.x[data.batch==0,:self.cfg.input_node_features].cpu().numpy(), f'data_x_r{r[0]}.png')
-            COMM.Barrier()
+        #if self.cfg.postprocess and self.iteration%100 == 0:
+        #if self.cfg.postprocess and ((r == 10).any() or (r == 60).any()):
+        if self.cfg.postprocess:
+            #if (r == 10).any():
+            #    index = torch.where(r == 10)[0].item()
+            #elif (r == 60).any():
+            #    index = torch.where(r == 60)[0].item()
+            for index in range(len(r)):
+                postprocess.plot_2d_field(COMM, graph.pos, field_r[data.batch==index].cpu().numpy(), f'field_r_r{r[index]}_iter{self.iteration}.png')
+                postprocess.plot_2d_field(COMM, graph.pos, data.x[data.batch==index,:self.cfg.input_node_features].cpu().numpy(), f'data_x_r{r[index]}_iter{self.iteration}.png')
+                COMM.Barrier()
         
         # Prediction
         tic = time.time()
@@ -1147,9 +1154,13 @@ class DGNTrainer:
                              SIZE = SIZE,
                              cond_node_features = None, # TODO: Add conditional node features
                              batch = data.batch)
-        if self.cfg.postprocess and self.iteration%100 == 0:
-            postprocess.plot_2d_field(COMM, graph.pos, model_noise[data.batch==0].detach().cpu().numpy(), f'model_noise_r{r[0]}.png')
-            postprocess.plot_2d_field(COMM, graph.pos, noise[data.batch==0].cpu().numpy(), f'noise_r{r[0]}.png')
+        if self.cfg.postprocess and ((r == 10).any() or (r == 60).any()):
+            if (r == 10).any():
+                index = torch.where(r == 10)[0].item()
+            elif (r == 60).any():
+                index = torch.where(r == 60)[0].item()
+            postprocess.plot_2d_field(COMM, graph.pos, model_noise[data.batch==index].detach().cpu().numpy(), f'model_noise_r{r[index]}_iter{self.iteration}.png')
+            postprocess.plot_2d_field(COMM, graph.pos, noise[data.batch==index].cpu().numpy(), f'noise_r{r[index]}_iter{self.iteration}.png')
             COMM.Barrier()
         if self.cfg.timers: self.update_timer('forwardPass', self.timer_step, time.time() - tic)
 
@@ -1188,7 +1199,8 @@ class DGNTrainer:
                     if RANK == 0:
                         log.info(f"MSE loss term: {mse_term.detach().cpu().numpy().tolist()}, mean = {mse_term.detach().cpu().mean().numpy().tolist()}")
                         log.info(f"VLB loss term: {vlb_term.detach().cpu().numpy().tolist()}, mean = {vlb_term.detach().cpu().mean().numpy().tolist()}")
-            loss = (loss * importance_weights).mean()
+            #loss = (loss * importance_weights).mean() # this undoes bias of sampling, not what we want generally
+            loss = loss.mean()
         else: # custom consistent loss
             # TODO: Custom parallel and consistent loss function to be implemented here
             if RANK == 0: log.error('Custom parallel and consistent loss not yet implemented')