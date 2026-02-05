ed76e74c4076b0895c31d68314027491be557af3
Riccardo <riccardo.balin@gmail.com>
2026-02-05 20:26:00 +0000

    Minor change to run_all2all_bench.sh

--- 3rd_party/dist-gnn/run_all2all_bench.sh
+++ 3rd_party/dist-gnn/run_all2all_bench.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
+#!/bin/bash -l
+##PBS -S /bin/bash
+##PBS -N a2av_bench
+##PBS -l select=8
+##PBS -l walltime=00:30:00
+##PBS -l filesystems=home
+##PBS -A datascience
+##PBS -q workq
+##PBS -k doe
+##PBS -j oe
+#cd $PBS_O_WORKDIR
 
 SYSTEM="aurora"
 
@@ -10,11 +20,11 @@ if [ ${SYSTEM} == "aurora" ]; then
   export CCL_PROCESS_LAUNCHER=pmix
   export CCL_ATL_TRANSPORT=mpi
   export CCL_KVS_MODE=mpi
-  #export CCL_ENABLE_SYCL_KERNELS=1
+  export CCL_ENABLE_SYCL_KERNELS=1
   export CCL_ALLTOALLV=topo
   export CCL_ALLTOALLV_SCALEOUT=topo
   export CCL_ALLTOALLV_MONOLITHIC_KERNEL=0
-  #export CCL_CONFIGURATION=cpu_gpu_dpcpp
+  export CCL_CONFIGURATION=cpu_gpu_dpcpp
   #export CCL_LOG_LEVEL=debug
   #unset ONEAPI_DEVICE_SELECTOR
 
@@ -25,8 +35,7 @@ if [ ${SYSTEM} == "aurora" ]; then
   #unset CCL_ROOT
   #export CCL_CONFIGURATION_PATH=""
   #export CCL_CONFIGURATION=cpu_gpu_dpcpp
-  #export CCL_ROOT="/lus/flare/projects/datasets/softwares/oneccl/oneccl_main_93f2621_oneapi_2025.2.0_umd_1146.12/oneCCL/oneccl_main_93f2621_oneapi_2025.2.0_umd_1146.12/_install"
-  #export CCL_ROOT="/soft/compilers/oneapi/2025.3.0/oneapi/ccl/latest"
+  #export CCL_ROOT="/home/balin/ALCF-4/oneCCL/build/_install"
   #export LD_LIBRARY_PATH=${CCL_ROOT}/lib:$LD_LIBRARY_PATH
   #export CPATH=${CCL_ROOT}/include:$CPATH
   #export LIBRARY_PATH=${CCL_ROOT}/lib:$LIBRARY_PATH
