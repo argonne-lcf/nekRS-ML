8eaf399ca1d2e3810c794775ef7fc3a6b7058649
Thilina Ratnayaka <thilinarmtb@gmail.com>
2026-03-05 02:36:23 +0000

    Update nrsrun_* scripts and unify affinity_nrs.sh and affinity_ml.sh

--- scripts/ml/polaris.sh
+++ scripts/ml/polaris.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 num_gpus=$1
+offset=$2
 shift
-gpu_id=$((PALS_LOCAL_RANKID % ${num_gpus} ))
+shift
+gpu_id=$(( PALS_LOCAL_RANKID % ${num_gpus} + ${offset} ))
 export CUDA_VISIBLE_DEVICES=$gpu_id
 exec "$@"
