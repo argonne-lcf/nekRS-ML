9f72691b1c357b1ad44f952f04d4a3cb8e62da1c
Riccardo Balin <riccardo.balin@gmail.com>
2026-05-08 16:01:31 +0000

    Fixes to get hill scaling factor correctly through to userdat2

--- examples/periodicHill_ensemble/test.sh
+++ examples/periodicHill_ensemble/test.sh
@@ -1,4 +1,8 @@
- export NEKRS_HOME=/flare/AdvanceFusionFission/balin/nekRS-ML/exe
- export OCCA_DPCPP_COMPILER_FLAGS="-O3 -fsycl -fsycl-targets=intel_gpu_pvc -ftarget-register-alloc-mode=pvc:auto -fma"
- mpiexec -n 12 --ppn 12 --cpu-bind=list:1:8:16:24:32:40:53:60:68:76:84:92 -- $NEKRS_HOME/bin/nekrs --setup periodicHill --backend dpcpp --build-only
- mpiexec -n 12 --ppn 12 --cpu-bind=list:1:8:16:24:32:40:53:60:68:76:84:92 -- $NEKRS_HOME/bin/nekrs --setup periodicHill --backend dpcpp 
+#!/bin/bash 
+
+module load frameworks
+
+export NEKRS_HOME=/flare/AdvanceFusionFission/balin/nekRS-ML/exe
+export OCCA_DPCPP_COMPILER_FLAGS="-O3 -fsycl -fsycl-targets=intel_gpu_pvc -ftarget-register-alloc-mode=pvc:auto -fma"
+#mpiexec -n 12 --ppn 12 --cpu-bind=list:1:8:16:24:32:40:53:60:68:76:84:92 -- $NEKRS_HOME/bin/nekrs --setup periodicHill --backend dpcpp --build-only
+mpiexec -n 12 --ppn 12 --cpu-bind=list:1:8:16:24:32:40:53:60:68:76:84:92 -- $NEKRS_HOME/bin/nekrs --setup periodicHill --backend dpcpp 2>&1 | tee nekrs.log
