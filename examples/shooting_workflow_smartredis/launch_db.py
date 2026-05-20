b8c6d673509652bd236ddb942adc67893460803c
Riccardo <riccardo.balin@gmail.com>
2026-05-20 20:12:55 +0000

    Remove run outputs

--- examples/shooting_workflow_smartredis/launch_db.py
+++
@@ -1,9 +0,0 @@
-from time import sleep
-from smartredis import Client
-client = Client(cluster=False)
-while True:
-    if client.key_exists('stop-coDB'):
-        print('Found stop-coDB',flush=True)
-        break
-    else:
-        sleep(5)
--- examples/shooting_workflow_smartredis/stop_db.py
+++
@@ -1,6 +0,0 @@
-from smartredis import Client
-import os
-import numpy as np
-SSDB = os.getenv('SSDB')
-client = Client(address=SSDB,cluster=False)
-client.put_tensor('stop-coDB',np.array([1]))
